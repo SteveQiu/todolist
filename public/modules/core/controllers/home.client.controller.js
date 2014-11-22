17102623b8fbaaed1e447eb20718e18c7c9534e7
SteveQiu <qiuyuliang20042005@hotmail.com>
2014-11-21 17:22:11 -0800

    modify home[age

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,17 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.page=0;
+		//$scope.pageUrl='modules/templates/views/create-template.client.view.html';
+		$scope.pageUrls=[
+			'modules/templates/views/create-template.client.view.html',
+			'modules/users/views/authentication/signin.client.view.html',
+			'modules/templates/views/list-templates.client.view.html'
+		];
+		$scope.pageUrl=$scope.pageUrls[0];
+		$scope.setPage= function(num){
+			$scope.page=num;
+			$scope.pageUrl=$scope.pageUrls[num];
+		};
 	}
 ]);
