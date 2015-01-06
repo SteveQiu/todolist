20f12a2ad7ca3837d934ee4e35dae97e550fc3d0
OminStyle <name.cmh@gmail.com>
2015-01-06 15:51:36 -0800

    use Yo generator to create teams CRUD module

--- app/models/team.server.model.js
+++ app/models/team.server.model.js
@@ -19,23 +19,11 @@ var TeamSchema = new Schema({
 	created: {
 		type: Date,
 		default: Date.now
-	}
-
-	/*
+	},
 	user: {
 		type: Schema.ObjectId,
 		ref: 'User'
-	},
-	taskList: [{
-		name: { type: String, default:'', required: 'Please fill Task name', trim: true},
-		isDone: { type: Boolean, default: false}
-	}],
-	active: {
-		type: Boolean,
-		default: true
 	}
-	*/
-	
 });
 
 mongoose.model('Team', TeamSchema);
