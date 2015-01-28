c3629d6c326c28099d8bb2a77539eb4dd55232f9
Madhatter-- <h.r.yang@hotmail.com>
2015-01-27 23:58:46 -0800

    WIP trying polling

--- config/express.js
+++ config/express.js
@@ -149,8 +149,8 @@ module.exports = function(db) {
 	// Attach and set up Socket.io
 	var server = http.createServer(app);
 	var io = socketio.listen(server);	
-  // io.set('transports', ['xhr-polling']);
-  // io.set('polling duration', 10);
+  io.set('transports', ['xhr-polling']);
+  io.set('polling duration', 10);
 	app.set('socketio', io);			// tie it up with the app instance so that we can access it from anywhere in our app.
 	app.set('server', server);		// storing the socket.io and the server instance in our app container.
 
