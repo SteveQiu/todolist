b627f2b166b612d148a5b66d2c0100de47036264
Madhatter-- <h.r.yang@hotmail.com>
2015-01-28 00:06:35 -0800

    WIP now sets server to listen based on env

--- config/express.js
+++ config/express.js
@@ -149,10 +149,11 @@ module.exports = function(db) {
 	// Attach and set up Socket.io
 	var server = http.createServer(app);
 	var io = socketio.listen(server);	
-  io.set('transports', ['xhr-polling']);
-  io.set('polling duration', 10);
+  // io.set('transports', ['xhr-polling']);
+  // io.set('polling duration', 10);
 	app.set('socketio', io);			// tie it up with the app instance so that we can access it from anywhere in our app.
 	app.set('server', server);		// storing the socket.io and the server instance in our app container.
+	server.listen(process.env.PORT || 3000);
 
 	// Export process.env.NODE_ENV to 'env'
 	app.get('/nodeenv', function(req, res, next){
