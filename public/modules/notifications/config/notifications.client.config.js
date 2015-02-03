ca146a5a1d53fce6f17273dd8f72cba8ddebd9d1
SteveQiu <qiuyuliang20042005@hotmail.com>
2015-02-03 12:24:49 -0800

    add notification crud

---
+++ public/modules/notifications/config/notifications.client.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('notifications').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Notifications', 'notifications', 'dropdown', '/notifications(/create)?');
+		Menus.addSubMenuItem('topbar', 'notifications', 'List notifications', 'notifications');
+		Menus.addSubMenuItem('topbar', 'notifications', 'New Checklist', 'checklists/create');
+	}
+]);
